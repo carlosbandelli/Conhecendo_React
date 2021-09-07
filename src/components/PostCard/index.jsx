8937e72b250b84f3cb76a1c2c6902fd318644db1
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-09-07 18:41:59 -0300

    paginaçãooo

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,11 +1,13 @@
-export const PostCard = ({title,cover,body}) =>  //----> 3ªº usando destructuring - pegando post diretamento de props
+import './styles.css'
+
+export const PostCard = ({title,cover,body,id}) =>  //----> 3ªº usando destructuring - pegando post diretamento de props
     //const post = props.post --> 1ª forma de fazer
     //const {post} = props --> 2ª forma de fazer usando destructuring
     (
         <div className="post">
           <img src={cover} alt={title}/>
           <div  className="post-content">
-            <h1>{title}</h1>
+            <h1>{title} {id}</h1>
             <p>{body}</p>
           </div>
         </div>
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,13 +4,17 @@ import { Component } from 'react';
 import './styles.css';
 
 import { Posts } from '../../components/Posts';
-import {loadPosts} from '../../utils/load-post'
+import {loadPosts} from '../../utils/load-post';
+import { Button } from '../../components/Button';
 
 
 
 export class Home extends Component {
   state = {    
-    posts: []
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10,
   };
 
   
@@ -19,18 +23,47 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {  
+    const { page, postsPerPage } = this.state
     const postsAndPhotos = await loadPosts()
-    this.setState({ posts: postsAndPhotos })
+    this.setState({ 
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+     })
   }
 
-  
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
+    posts.push(...nextPosts);
+
+    this.setState({posts, page: nextPage})
+
+    
+  }
 
   render() {
-    const { posts } = this.state
+    const { posts, page, postsPerPage, allPosts } = this.state
+    const noMorePosts = page + postsPerPage >= allPosts.length
 
     return (
      <section className="container">
         <Posts posts={ posts }/>
+
+        <div class="button-container">
+        <Button 
+        text="Load more posts"
+        onClick={this.loadMorePosts}
+        disabled={noMorePosts}
+
+        />
+        </div>
+      
      </section>      
     );
   }
