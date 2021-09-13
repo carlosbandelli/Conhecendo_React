11c7d7ceffc9b87baeb45e1bfb4f12c0a6924500
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-09-13 02:19:48 -0300

    Aplicação com teste

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -44,5 +44,10 @@ describe('<Button/>', () => {
         expect(button).toBeEnabled()
         
     })
+    it('should match snapshot', () => {
+        const fn = jest.fn()
+        const {container} = render (<Button text="Load more" disable={false} onClick={fn} />)
+        expect(container.firstChild).toMatchSnapshot()
+    })
 
 })
